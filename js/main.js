b48bda6e33039d78627a1646cd2b70706c660aa5
Md Aasim <mdaasim5266@gmail.com>
2021-09-12 00:00:39 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-(function ($) {
-    "use strict";
+// (function ($) {
+//     "use strict";
     
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+//     // Back to top button
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop() > 200) {
+//             $('.back-to-top').fadeIn('slow');
+//         } else {
+//             $('.back-to-top').fadeOut('slow');
+//         }
+//     });
+//     $('.back-to-top').click(function () {
+//         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+//         return false;
+//     });
     
     
     // Sticky Navbar
