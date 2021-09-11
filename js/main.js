dce3adeaceadf19d9546c9c71499446ef2803da2
Md Aasim <mdaasim5266@gmail.com>
2021-09-12 00:05:10 +0530

    1.3

--- js/main.js
+++ js/main.js
@@ -1,20 +1,5 @@
-// (function ($) {
-//     "use strict";
-    
-//     // Back to top button
-//     $(window).scroll(function () {
-//         if ($(this).scrollTop() > 200) {
-//             $('.back-to-top').fadeIn('slow');
-//         } else {
-//             $('.back-to-top').fadeOut('slow');
-//         }
-//     });
-//     $('.back-to-top').click(function () {
-//         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-//         return false;
-//     });
-    
-    
+(function ($) {
+
     // Sticky Navbar
     $(window).scroll(function () {
         if ($(this).scrollTop() > 0) {
@@ -23,25 +8,6 @@
             $('.navbar').removeClass('nav-sticky');
         }
     });
-    
-    
-    // Dropdown on mouse hover
-    $(document).ready(function () {
-        function toggleNavbarMethod() {
-            if ($(window).width() > 992) {
-                $('.navbar .dropdown').on('mouseover', function () {
-                    $('.dropdown-toggle', this).trigger('click');
-                }).on('mouseout', function () {
-                    $('.dropdown-toggle', this).trigger('click').blur();
-                });
-            } else {
-                $('.navbar .dropdown').off('mouseover').off('mouseout');
-            }
-        }
-        toggleNavbarMethod();
-        $(window).resize(toggleNavbarMethod);
-    });
-
     
     // Main carousel
     $(".carousel .owl-carousel").owlCarousel({
@@ -55,32 +21,6 @@
         nav : false
     });
     
-    // Modal Video
-    $(document).ready(function () {
-        var $videoSrc;
-        $('.btn-play').click(function () {
-            $videoSrc = $(this).data("src");
-        });
-        console.log($videoSrc);
-
-        $('#videoModal').on('shown.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-        })
-
-        $('#videoModal').on('hide.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc);
-        })
-    });
-    
-    
-    // Date and time picker
-    $('#date').datetimepicker({
-        format: 'L'
-    });
-    $('#time').datetimepicker({
-        format: 'LT'
-    });
-
 
     // Testimonials carousel
     $(".testimonials-carousel").owlCarousel({
@@ -100,30 +40,6 @@
             },
             992:{
                 items:3
-            }
-        }
-    });
-    
-    
-    // Related post carousel
-    $(".related-slider").owlCarousel({
-        autoplay: true,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
             }
         }
     });
